e0cd8807904d6071d75584cef112e0277ae0db14
akhilkannur <44062401+akhilkannur@users.noreply.github.com>
2025-07-19 22:11:39 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js'
 
 const supabaseUrl = 'https://egdotoxdqmemjjdsgvhp.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnZG90b3hkcW1lbWpqZHNndmhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5NDAyNjgsImV4cCI6MjA2ODUxNjI2OH0.-soEkqAvjIx7cov9bCIbXznKGTrIAozvpgptRIbyrW0';
-
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnZG90b3hkcW1lbWpqZHNndmhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5NDAyNjgsImV4cCI6MjA2ODUxNjI2OH0.-soEkqAvjIx7cov9bCIbXznKGTrIAozvpgptRIbyrW0'; // your anon key
 export const supabase = createClient(supabaseUrl, supabaseKey);
